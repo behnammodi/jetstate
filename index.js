253b53bac6994f754b19b92c87a81a8840fc2109
Hossein Mohammadi <hotman.hh@gmail.com>
2018-12-20 14:23:17 +0330

    improve jetstate ,init return a class to work better

--- index.js
+++ index.js
@@ -1,38 +1,89 @@
 "use strict";
-
+const { emit, on } = require("jetemit");
 exports.__esModule = true;
-var state = {
-  __container__: {},
-};
+var state = {};
 
+class Action {
+  constructor(config) {
+    this.data = config.defaultValue;
+    this.name = config.name || new Date().getTime().toString();
+    this._willReceive = config.willReceive;
+    this._willUpdate = config.willUpdate;
+    this._shouldUpdate = config.shouldUpdate;
+    this.addDidUpdate(config.didUpdate);
+    config.name &&
+      Object.defineProperty(state, config.name, {
+        get: this.get,
+        set: this.set
+      });
+  }
+  /**
+   * @param {function} callback function for before get state
+   */
+  set willReceive(callback) {
+    this._willReceive = callback;
+  }
+  /**
+   * @param {function} callback function for before set state
+   */
+  set willUpdate(callback) {
+    this._willUpdate = callback;
+  }
+  /**
+   * @param {function} callback function for is need update
+   */
+  set shouldUpdate(callback) {
+    this._shouldUpdate = callback;
+  }
+  set = value => {
+    let newData = null;
+    if (typeof this.data == "object" && typeof value == "object")
+      newData = { ...this.data, ...value };
+    else newData = value;
+
+    this._willUpdate && this._willUpdate(this.data, newData);
+    if (this._shouldUpdate) if (!this._shouldUpdate(this.data, newData)) return;
+    emit(this.name, newData); //run didupdate functions
+    this.data = newData;
+  };
+  get = key => {
+    this._willReceive && this._willReceive(this.data);
+
+    if (key)
+      if (typeof this.data == "object") return this.data[key];
+      else return undefined;
+    return this.data;
+  };
+  remove(key) {
+    if (key) {
+      this.data[key] && delete this.data[key];
+    } else this.data = null;
+    return this.data;
+  }
+  /**
+   * @param {function} callback function for after set state
+   * @returns {function} function for unsubscribe
+   */
+  addDidUpdate(callback) {
+    return on(this.name, callback);
+  }
+}
 /**
  * init new state
- * @param {object} config 
+ * @param {object} config
  * @param {string} config.name name state
- * @param {any} config.defaultValue default value 
+ * @param {any} config.defaultValue default value
  * @param {function} config.willReceive function for before get state
  * @param {function} config.willUpdate function for before set state
  * @param {function} config.shouldUpdate function for is need update
- * @param {function} config.didUpdate function for after set state
- * @returns {undefined} nothing
+ * @param {function} config.didUpdate function for is need update
+ * @returns {Object} state object for work with this state
  */
 exports.init = function init(config) {
-  state.__container__[config.name] = config.defaultValue;
-  Object.defineProperty(state, config.name, {
-    get: function get() {
-      config.willReceive && config.willReceive(state.__container__[config.name]);
-      return state.__container__[config.name];
-    },
-    set: function set(value) {
-      config.willUpdate && config.willUpdate(state.__container__[config.name], value);
-      if (config.shouldUpdate)
-        if (config.shouldUpdate(state.__container__[config.name], value) === false) return;
-      state.__container__[config.name] = value;
-      config.didUpdate && config.didUpdate(value);
-    }
-  });
-}
-
+  config = typeof config == "object" ? config : {};
+  const action = new Action(config);
+  return action;
+};
 /**
  * state container
  */
--- readme.md
+++ readme.md
@@ -1,21 +1,27 @@
 # jetstate
+
 state managment
 
 ## install
+
 ```npm
 npm i jetstate --save
 ```
 
 ## use
+
 ```javascript
 import { init, state } from 'jetstate';
 
 /**
  * initial new state
  */
-init({
+var time=init({
   name: 'time',
-  defaultValue: new Date().getTime(),
+  defaultValue: {
+    minutes:new Date().getMinutes(),
+    hours:new Date().getHours()
+  },
   willReceive: value => {
     console.log('willReceive time', value);
   },
@@ -23,17 +29,48 @@ init({
     console.log('willUpdate', previousValue, nextValue);
   },
   shouldUpdate: (previousValue, nextValue) => {
-    if (typeof nextValue 'number') return true;
-    else return false        
+    if (typeof nextValue 'object') return true;
+    else return false
   },
   didUpdate: value => {
     console.log('didUpdate', value);
   }
 });
-
+..
 // with this code, willReceive is run
-var time = state.time;
+state.time;
+// or
+time.get();
+// return {hours,minutes}
+..
+// you can get an item
+time.get("hours");
+// return {hours,minutes}
+..
 
-//with this code, willUpdate is run and then didUpdate is run
-state.time = new Date().getTime();
+//with this code, willUpdate is run and then all added didUpdate is run
+state.time = {minutes:new Date().getMinutes()};
+//or
+time.set({minutes:new Date().getMinutes()})
+//now state time is {hours,minutes}
+..
+// you can remove an item
+time.remove("hours");
+// return {minutes}
+```
+
+## change inited
+
+```javascript
+// change willReceive
+time.willReceive(function(value){
+  console.log('new will receive', value);
+})
+
+..
+// add another didUpdate
+time.addDidUpdate(function(value){
+  console.log('second didUpdate', value);
+})
+//when set state all added didUpdate is run
 ```
