e2dc7aabf0f07dfe9edb1a8c521a697745c506f3
Behnam Mohammadi (بهنام محمدی) <itten@live.com>
2019-01-09 10:58:51 +0330

    removed willReceive method

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ var state = {
 };
 
 /**
- * init new state
+ * @description init new state
+ * @public
+ * @version 1.2.0
  * @param {object} config 
  * @param {string} config.name name state
  * @param {any} config.defaultValue default value 
- * @param {function} config.willReceive function for before get state
  * @param {function} config.willUpdate function for before set state
  * @param {function} config.shouldUpdate function for is need update
  * @param {function} config.didUpdate function for after set state
@@ -19,8 +20,7 @@ var state = {
 exports.init = function init(config) {
   state.__container__[config.name] = config.defaultValue;
   Object.defineProperty(state, config.name, {
-    get: function get() {
-      config.willReceive && config.willReceive(state.__container__[config.name]);
+    get: function get() {      
       return state.__container__[config.name];
     },
     set: function set(value) {
@@ -34,6 +34,8 @@ exports.init = function init(config) {
 }
 
 /**
- * state container
+ * @description state container
+ * @public
+ * @version 1.2.0
  */
 exports.state = state;
